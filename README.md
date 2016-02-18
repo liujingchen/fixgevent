7d475f0ed462e31607d7275dfe115c2738746792
Jingchen Liu <liu@indeed.com>
2016-02-18 12:40:43 +0900

    reproduce the bug

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# Reproduce the bug of gevent's monkey_patch breaking multiprocess.
+Reproduce the bug of gevent's monkey_patch breaking multiprocess.
+```
+pip install gevent
+
+python run.py  # success
+python run.py break  # fail due to the bug
+```
---
+++ run.py
@@ -0,0 +1,32 @@
+from multiprocessing import Manager, Process
+from gevent.monkey import patch_all
+from time import sleep
+import sys
+
+if sys.argv[0] == 'break':
+    patch_all()
+manager = Manager()
+shared_dict = manager.dict()
+shared_dict['count'] = 0
+stop_event = manager.Event()
+
+
+def other_process_run():
+    while True:
+        if stop_event.is_set():
+            print "child process exit"
+            break
+        count = shared_dict['count']
+        shared_dict['count'] = count + 1
+        sleep(1)
+
+p = Process(target=other_process_run)
+p.start()
+
+for i in range(0, 10):
+    sleep(1)
+    print shared_dict.get('count')
+
+stop_event.set()
+p.join()
+print "main process exit"
