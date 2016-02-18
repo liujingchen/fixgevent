ab3638f63b6658067b3b76ac83cd70179a7d42eb
Jingchen Liu <liu@indeed.com>
2016-02-18 17:52:08 +0900

    set up log

--- run.py
+++ run.py
@@ -1,7 +1,16 @@
 from multiprocessing import Manager, Process
+import multiprocessing.util as util
 from gevent.monkey import patch_all
 from time import sleep
+from logging import StreamHandler, Formatter
 import sys
+logger = util.get_logger()
+logger.setLevel("DEBUG")
+handler = StreamHandler(sys.stdout)
+_DEFAULT_FORMAT = '%(asctime)s %(levelname)s %(name)s: %(message)s [in %(pathname)s:%(lineno)d, ' \
+                  '%(thread)d (%(threadName)s), %(process)d (%(processName)s)]'
+handler.setFormatter(Formatter(_DEFAULT_FORMAT))
+logger.addHandler(handler)
 
 if len(sys.argv) > 1 and sys.argv[1] == 'break':
     patch_all()
@@ -14,7 +23,7 @@ stop_event = manager.Event()
 def other_process_run():
     while True:
         if stop_event.is_set():
-            print "child process exit"
+            logger.info("child process exit")
             break
         count = shared_dict['count']
         shared_dict['count'] = count + 1
@@ -25,8 +34,8 @@ p.start()
 
 for i in range(0, 10):
     sleep(1)
-    print shared_dict.get('count')
+    logger.info(str(shared_dict.get('count')))
 
 stop_event.set()
 p.join()
-print "main process exit"
+logger.info("main process exit")
